da021d7650364cf6bfea734df21c1463695083db
MTRNord <mtrnord1@gmail.com>
2022-10-10 21:58:20 +0200

    Listen on 0.0.0.0

--- crates/model_server/src/main.rs
+++ crates/model_server/src/main.rs
@@ -43,7 +43,7 @@ async fn main() -> Result<()> {
         .route("/submit", post(submit))
         .route("/submit_review", post(submit_for_review));
 
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
     info!("listening on {}", addr);
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
